25556c6f7d836b0623c2194f20e690afcda35d0c
steffen <steffen@agenturwerk.de>
2024-09-13 12:08:18 +0200

    first commit

---
+++ app/components/StairTransition.jsx
@@ -0,0 +1,7 @@
+const StairEffect = () => {
+    return (
+        <div className={''}>test</div>
+    );
+}
+
+export default StairEffect;
---
+++ app/services/page.tsx
@@ -0,0 +1,9 @@
+import Link from 'next/link';
+export default function AboutPage() {
+    return (
+        <div>
+            <h1>Contact Us</h1>
+            <p>This is the contact page. You can put contact details here.</p>
+        </div>
+    );
+}
