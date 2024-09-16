69b75b73a2839e68dfe36551fc8d0862cfbb5e7b
steffen <steffen@agenturwerk.de>
2024-09-16 15:13:47 +0200

    new NextLink

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,5 +1,5 @@
 "use client";
-import Link from "next/link";
+import NextLink from "next/link";
 import { usePathname} from "next/navigation";
 
 const links = [
@@ -32,8 +32,8 @@ const Nav = () => {
         <nav className="flex gap-8">
             {links.map((link, index)=> {
                 return (
-                     <Link href={link.path} key={index} className={`${link.path === pathName && "text-accent border-b-2 border-accent" } 
-                     capitalize font-medium hover:text-accent transition-all"}`}>{link.name}</Link>
+                     <NextLink href={link.path} key={index} className={`${link.path === pathName && "text-accent border-b-2 border-accent" } 
+                     capitalize font-medium hover:text-accent transition-all"}`}>{link.name}</NextLink>
                 );
             })}
         </nav>
