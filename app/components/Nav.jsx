66b616ce5d377a1e45946e866a4f2b60d25df83f
steffen <steffen@agenturwerk.de>
2024-09-16 15:18:32 +0200

    test

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,42 +1,11 @@
 "use client";
-/*import NextLink from "next/link";*/
-import { usePathname} from "next/navigation";
 
-const links = [
-    {
-        name: "home",
-        path: "/",
-    },
-    {
-        name: "services",
-        path: "/services",
-    },
-    {
-        name: "resume",
-        path: "/resume",
-    },
-    {
-        name: "work",
-        path: "/work",
-    },
-    {
-        name: "contact",
-        path: "/contact",
-    },
-]
 
 const Nav = () => {
-    const pathName = usePathname()
+
 
     return (
-        <nav className="flex gap-8">
-            {links.map((link, index)=> {
-                return (
-                     <a href={link.path} key={index} className={`${link.path === pathName && "text-accent border-b-2 border-accent" } 
-                     capitalize font-medium hover:text-accent transition-all"}`}>{link.name}</a>
-                );
-            })}
-        </nav>
+        <div>test</div>
     );
 }
 
