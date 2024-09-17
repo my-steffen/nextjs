86a39050b966d419f2a6ea0ea93654e55fec18d7
steffen <steffen@agenturwerk.de>
2024-09-17 09:03:26 +0200

    ICONS and Resume

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,6 +1,58 @@
 "use client";
 
-import {FaHtml5, FaCss3, FaJs, FaReact, FaNodeJs} from "react-icons/fa"
+import {
+    FaHtml5,
+    FaCss3,
+    FaJs,
+    FaReact,
+    FaNodeJs
+    } from "react-icons/fa";
+
+import { SiTailwindcss, SiNextdotjs } from "react-icons/si";
+
+//about
+const about = {
+    title: "About me",
+    description: "Lorem Upsum",
+    info: [
+        {
+            fieldName: "Name",
+            fieldValue: ""
+        },
+        {
+            fieldName: "Phone",
+            fieldValue: "1234"
+        },
+        {
+            fieldName: "Experience",
+            fieldValue: "12+ Year"
+        },
+        {
+            fieldName: "Insta",
+            fieldValue: "steff"
+        },
+        {
+            fieldName: "Nationality",
+            fieldValue: "German"
+        },
+        {
+            fieldName: "Email",
+            fieldValue: "steffen@test.de"
+        },
+        {
+            fieldName: "Languages",
+            fieldValue: "English, Deutsch"
+        },
+    ]
+}
+
+const experience = {
+    icon: '/assets/resume/badge.svg',
+    title: 'My Experience',
+    description: 'Lorem Ipsum'
+}
+
+
 export default function Resume() {
     return (
         <div>
